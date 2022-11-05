71c17d9090ce098e05c3e65fdccbe3f689695791
Ridwan Efendi <ridwan.efen190301@gmail.com>
2022-11-05 19:40:33 +0700

    tamplate

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+// const routes = require('./routes')
+
+const app = express()
+const port = 8000
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        status: true,
+        message: 'Halo ini API dari TodoList Prisma'
+    })
+})
+
+// routes(app)
+
+app.listen(port, () => {
+    console.log(`Server running di http://localhost:${port}`)
+})
+
---
+++ index.js
@@ -0,0 +1,68 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt= require('bcrypt')
+
+const prisma = new PrismaClient();
+
+async function main() {
+    // await prisma.user.create({
+    //     data: {
+    //         name: 'Rangga',
+    //         email: 'rangga@gmail.com',
+    //         password: bcrypt.hashSync('Rangga12345', 10),
+    //         todo: {
+    //         }
+    //     },
+    // }),
+
+    // await prisma.user.create({
+    //     data: {
+    //         name: 'Rini',
+    //         email: 'Rini@gmail.com',
+    //         password: bcrypt.hashSync('Rini12345', 10),
+    //         todo: {
+    //         }
+    //     },
+    // })
+
+    // await prisma.user.create({
+    //     data: {
+    //         name: 'Surya',
+    //         email: 'Surya@gmail.com',
+    //         password: bcrypt.hashSync('Surya12345', 10),
+    //         todo: {
+    //         }
+    //     },
+    // })
+    
+    await prisma.rekening.create({
+        data: {
+            userId: 1
+        },
+    })
+
+    await prisma.rekening.create({
+        data: {
+            userId: 2
+        },
+    })
+
+    await prisma.rekening.create({
+        data: {
+            userId: 3
+        },
+    })
+
+    const allUsers = await prisma.user.findMany()
+
+    console.dir(allUsers, { depth: null })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
